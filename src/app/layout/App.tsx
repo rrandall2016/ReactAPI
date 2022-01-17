d54c54870541a2e30ca4ace28b3c8b5aa68bd9e7
RRandall <rrandall_2001@yahoo.com>
2022-01-17 12:28:30 -0800

    Add files via upload

---
+++ src/app/layout/App.tsx
@@ -0,0 +1,44 @@
+
+import { Container, CssBaseline, createTheme, ThemeProvider} from "@mui/material";
+import { useState } from "react";
+import { Route } from "react-router-dom";
+import AboutPage from "../../features/about/AboutPage";
+import Catalog from "../../features/catalog/Catalog";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import ContactPage from "../../features/contact/ContactPage";
+import HomePage from "../../features/home/HomePage";
+import Header from "./header";
+
+//npm start to run app
+function App() {
+  const[darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === 'light' ? '#eaeaea' : '#121212'
+      }
+    }
+  })
+
+  function handleThemeChange(){
+    setDarkMode(!darkMode);
+  }
+  return (
+    <ThemeProvider theme={theme}>
+    <CssBaseline/>
+        <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
+        <Container>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/catalog' component={Catalog} />
+          <Route path='/catalog/:id' component={ProductDetails} />
+          <Route path='/about' component={AboutPage} />
+          <Route path='/contact' component={ContactPage} />
+        </Container>
+      
+    </ThemeProvider>
+  );
+}
+
+export default App;
